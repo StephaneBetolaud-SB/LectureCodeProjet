78c8d5b48757831cdfaabd27e607de360af922e1
StephaneBetolaud-SB <64788985+StephaneBetolaud-SB@users.noreply.github.com>
2020-05-05 11:11:54 +0200

    Commit developpements tokenizer 05/05/2020 11:11

--- src/test/PremierTest.java
+++ src/test/PremierTest.java
@@ -2,6 +2,7 @@ package test;
 
 public class PremierTest {
 
-	// classe qui sert a ecrire du code java et de faire les tests de bon fonctionnement sur notre main. 
-	// Ce n'est pas grave si la classe ne compile pas car c'est l'objectif.
+	
+	private String bonsoir;
+	
 }
--- src/token/Tokenizer.java
+++ src/token/Tokenizer.java
@@ -5,10 +5,33 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Tokenizer {
 
 	// etape 1 : traiter les lignes du fichiers pour virer les espaces et \n
+	
+	// variables globales statiques
+	
+	// properties
+	private static final String propertyPublic = "public";
+	private static final String propertyPrivate = "private";
+	private static final String propertyStatic = "static";
+	private static final String propertyProtected = "protected";
+	
+	// types
+	
+	private static final String typeBool = "boolean";
+	private static final String typeInt = "int";
+	private static final String typeInteger = "Integer";
+	private static final String typeBoolean = "Boolean";
+	private static final String typeFloat = "float";
+	private static final String typeDouble = "double";
+	private static final String typeString = "string";
+	private static final String typeChar = "char";
+	
 	
 	public Tokenizer() {
 		
@@ -21,7 +44,11 @@ public class Tokenizer {
 			while ((line = br.readLine()) != null) {
 			   // process the line.
 				
-				System.out.println(line);
+				StringTokenizer st = new StringTokenizer(line);
+			     while (st.hasMoreTokens()) {
+			         System.out.println(st.nextToken());
+			     }
+				
 				
 			}
 			br.close();
@@ -30,5 +57,28 @@ public class Tokenizer {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	// initialisation de la liste des types
+	private List<String> initTypes(){
+		List<String> retour = new ArrayList<String>();
+		
+		retour.add(typeBool);
+		retour.add(typeInt);
+		retour.add(typeInteger);
+		retour.add(typeBoolean);
+		retour.add(typeFloat);
+		retour.add(typeDouble);
+		retour.add(typeString);
+		retour.add(typeChar);
+		
+		return retour;
+	}
+	
+	public List<String> initProperties(){
+		List<String> retour = new ArrayList<String>();
+		
+		
+		return retour;
 	}
 }
