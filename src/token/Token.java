7ed9917ce7af1566f1b041328085b5ea31dc8f14
StephaneBetolaud-SB <64788985+StephaneBetolaud-SB@users.noreply.github.com>
2020-05-05 14:20:37 +0200

    Commit class Token

---
+++ src/token/Token.java
@@ -0,0 +1,53 @@
+package token;
+
+public class Token {
+	
+	private String type;
+	private String informationToken;
+	private String valeur;
+	private String nom;
+	
+	public Token(String nom,String informationToken, String type, String valeur) {
+		this.nom = nom;
+		this.type = type;
+		this.valeur = valeur;
+		this.informationToken = informationToken;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(String valeur) {
+		this.valeur = valeur;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getInformationToken() {
+		return informationToken;
+	}
+
+	public void setInformationToken(String informationToken) {
+		this.informationToken = informationToken;
+	}
+	
+	public String toString() {
+		return "token de nom : " + this.nom + ", l'information du token est : " + this.informationToken + ", le type : " + this.type + " et de valeur : " + this.valeur;
+	}
+
+}
