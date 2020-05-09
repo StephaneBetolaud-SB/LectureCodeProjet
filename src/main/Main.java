3f4e8eafeb65a7a8abd717fccc56749f4d5c3a63
yassin <yelmochrik@beable.fr>
2020-05-09 15:29:52 +0200

    Add Expression

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,10 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import token.Token;
+import expression.Expression;
 
 import token.Tokenizer;
 
@@ -14,12 +16,21 @@ public class Main {
 		
 		Tokenizer token = new Tokenizer();
 		
-		File file = new File("..\\LectureCodeProjet\\src\\test\\PremierTest.java");
+		// chemin pour YASSIN : /home/yassin/eclipse-workspace/LectureCodeProjet/src/test/PremierTest.java
+		
+		File file = new File("/home/yassin/eclipse-workspace/LectureCodeProjet/src/test/PremierTest.java");
 		// try de la methode tokenizer
 		try {
 			List<Token> tokens = token.tokenizer(file);
+			List<Expression> expressions = new ArrayList<Expression>();
+			Expression e = new Expression();
 			for(Token t : tokens ) {
-				System.out.println(t.toString());
+				//System.out.println(t.toString());
+				expressions.add(e.evaluateToken(t));
+			}
+			System.out.println(expressions.size());
+			for (Expression elem : expressions) {
+				System.out.println(elem.toString());
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
